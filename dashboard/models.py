13f86600ffc34ff77140e37cab4ebb8e7f341508
AhnafShahid <66224231+AhnafShahid@users.noreply.github.com>
2024-03-27 18:19:46 -0400

    Products to database

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+CATEGORY = (
+    ('Produce', 'Produce'),
+    ('Dairy', 'Dairy'),
+    ('Meat', 'Meat'),
+    ('Snacks', 'Snacks')
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+    category = models.CharField(max_length=50, choices=CATEGORY, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
