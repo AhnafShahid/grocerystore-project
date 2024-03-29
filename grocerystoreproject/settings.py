1e706a07c53e0a5193570a2dac492f7dca6d18da
AhnafShahid <66224231+AhnafShahid@users.noreply.github.com>
2024-03-29 18:51:16 -0400

    Profile stuff

--- grocerystoreproject/settings.py
+++ grocerystoreproject/settings.py
@@ -126,6 +126,10 @@ STATIC_ROOT = (BASE_DIR/"asert/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+MEDIA_ROOT = (BASE_DIR/'media')
+
+MEDIA_URL = '/media/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'dashboard-index'
